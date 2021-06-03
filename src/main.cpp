dc110be0e01d7c3e7b34f536bf29b0ab527887e4
Daniel Stölzner <stoelzner.daniel@gmail.com>
2021-06-03 14:12:44 +0800

    Fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,11 @@
 /*
-
 	This is a simple MJPEG streaming webserver implemented for AI-Thinker ESP32-CAM
 	and ESP-EYE modules.
 	This is tested to work with VLC and Blynk video widget and can support up to 10
 	simultaneously connected streaming clients.
 	Simultaneous streaming is implemented with FreeRTOS tasks.
-
 	Inspired by and based on this Instructable: $9 RTSP Video Streamer Using the ESP32-CAM Board
 	(https://www.instructables.com/id/9-RTSP-Video-Streamer-Using-the-ESP32-CAM-Board/)
-
 	Board: AI-Thinker ESP32-CAM or ESP-EYE
 	Compile as:
 	 ESP32 Dev Module
@@ -35,6 +32,9 @@
 #include <driver/rtc_io.h>
 #include <ArduinoJson.h>
 #include <WebOTA.h>
+#include <Preferences.h>
+
+Preferences preferences;
 
 // Select camera model
 //#define CAMERA_MODEL_WROVER_KIT
@@ -48,13 +48,11 @@
 /*
 	Next one is an include with wifi credentials.
 	This is what you need to do:
-
 	1. Create a file called "home_wifi_multi.h" in the same folder	 OR	 under a separate subfolder of the "libraries" folder of Arduino IDE. (You are creating a "fake" library really - I called it "MySettings").
 	2. Place the following text in the file:
 	#define SSID1 "replace with your wifi ssid"
 	#define PWD1 "replace your wifi password"
 	3. Save.
-
 	Should work then
 */
 #include "home_wifi_multi.h"
@@ -67,14 +65,19 @@ void handleJPGSstream(void);
 void streamCB(void * pvParameters);
 void camCB(void* pvParameters);
 char* allocateMemory(char* aPtr, size_t aSize);
+
 void handleJPG(void);
 
 void set_handler();
 void get_handler();
-void control_handler();
+
+void control3_handler();
 void restart_handler();
 void activatewebota_handler();
+void reset_handler();
 void handleNotFound();
+
+unsigned int counter = 0;
 
 //HTML-----------------------------------
 static const char PROGMEM INDEX_HTML[] = R"rawliteral(
@@ -100,15 +103,13 @@ body{font-family:Arial,Helvetica,sans-serif;background:#181818;color:#EFEFEF;fon
 												<div class="input-group" id="framesize-group">
 														<label for="framesize">Resolution</label>
 														<select id="framesize" class="default-action">
-																<option value="10">UXGA(1600x1200)</option>
-																<option value="9">SXGA(1280x1024)</option>
-																<option value="8">XGA(1024x768)</option>
-																<option value="7">SVGA(800x600)</option>
-																<option value="6">VGA(640x480)</option>
-																<option value="5" selected="selected">CIF(400x296)</option>
-																<option value="4">QVGA(320x240)</option>
-																<option value="3">HQVGA(240x176)</option>
-																<option value="0">QQVGA(160x120)</option>
+																<option value="13">1600x1200</option>
+																<option value="12">1280x1024</option>
+																<option value="11">1280x768</option>
+																<option value="10">1024x768</option>
+																<option value="9">800x600</option>
+																<option value="8">640x480</option>
+
 														</select>
 												</div>
 												<div class="input-group" id="quality-group">
@@ -332,12 +333,13 @@ body{font-family:Arial,Helvetica,sans-serif;background:#181818;color:#EFEFEF;fon
 							B.onclick = () => {
 									e(B.parentNode)
 							}
-					}), fetch(`${c}/get`).then(function (B) {
+					}), fetch('http://192.168.31.61/get?cachebuster=' + Date.now()).then(function (B) {
 							return B.json()
 					}).then(function (B) {
 							document.querySelectorAll('.default-action').forEach(C => {
 									i(C, B[C.id], !1)
-							})
+							});
+							setTimeout(function(){q();},500);
 					});
 					const j = document.getElementById('stream'),
 							k = document.getElementById('stream-container'),
@@ -380,7 +382,6 @@ body{font-family:Arial,Helvetica,sans-serif;background:#181818;color:#EFEFEF;fon
 					A.onchange = () => {
 							b(A), 5 < A.value && (i(y, !1), i(z, !1))
 					}
-					q();
 			});
 		</script>
 		</body>
@@ -444,11 +445,13 @@ void mjpegCB(void* pvParameters) {
 	//	Registering webserver handling routines
 	server.on("/mjpeg/1", HTTP_GET, handleJPGSstream);
 	server.on("/jpg", HTTP_GET, handleJPG);
-	server.on("/get", HTTP_GET, set_handler);
-	server.on("/set", HTTP_GET, get_handler);
-	server.on("/control", HTTP_GET, );
+	server.on("/get", HTTP_GET, get_handler);
+	server.on("/set", HTTP_GET, set_handler);
+
+	server.on("/control", HTTP_GET, control3_handler);
 	server.on("/restart", HTTP_GET, restart_handler);
 	server.on("/activatewebota", HTTP_GET, activatewebota_handler);
+	server.on("/reset", HTTP_GET, reset_handler);
 	server.onNotFound(handleNotFound);
 
 	//	Starting webserver
@@ -578,8 +581,8 @@ char* allocateMemory(char* aPtr, size_t aSize) {
 
 // ==== STREAMING ======================================================
 const char HEADER[] = "HTTP/1.1 200 OK\r\n" \
-											"Access-Control-Allow-Origin: *\r\n" \
-											"Content-Type: multipart/x-mixed-replace; boundary=123456789000000000000987654321\r\n";
+					  "Access-Control-Allow-Origin: *\r\n" \
+					  "Content-Type: multipart/x-mixed-replace; boundary=123456789000000000000987654321\r\n";
 const char BOUNDARY[] = "\r\n--123456789000000000000987654321\r\n";
 const char CTNTTYPE[] = "Content-Type: image/jpeg\r\nContent-Length: ";
 const int hdrLen = strlen(HEADER);
@@ -704,9 +707,15 @@ void handleNotFound(){
 	message += (server.method() == HTTP_GET) ? "GET" : "POST";
 	message += "\nArguments: ";
 	message += server.args();
+	message += "\nVersion: ";
+	message += "2";
+	message += "\n";
+	message += "\nCounter: ";
+	message += counter;
 	message += "\n";
 	server.send(200, "text / plain", message);
 }
+
 
 
 // ==== SETUP method ==================================================================
@@ -717,7 +726,12 @@ void setup(){
 	Serial.begin(115200);
 	delay(1000); // wait for a second to let Serial connect
 
-
+	preferences.begin("CameraSettings", false);
+	counter = preferences.getInt("counter", 0);
+	counter++;
+	preferences.putInt("counter", counter);
+	
+	
 	// Configure the camera
 	camera_config_t config;
 	config.ledc_channel = LEDC_CHANNEL_0;
@@ -757,10 +771,34 @@ void setup(){
 	}
 
 	sensor_t * s = esp_camera_sensor_get();
-	s->set_framesize(s, FRAMESIZE_XGA);
-	s->set_quality(s, 9);
-	s->set_gainceiling(s, (gainceiling_t)1);
 	
+	s->set_framesize(s, (framesize_t)preferences.getInt("framesize", s->status.framesize));
+	s->set_quality(s, 63 - preferences.getInt("quality", s->status.quality));
+	s->set_contrast(s, preferences.getInt("contrast", s->status.framesize));
+	s->set_brightness(s, preferences.getInt("brightness", s->status.brightness));
+	s->set_saturation(s, preferences.getInt("saturation", s->status.saturation));
+	s->set_gainceiling(s, (gainceiling_t)preferences.getInt("gainceiling", s->status.gainceiling));
+	s->set_colorbar(s, preferences.getInt("colorbar", s->status.colorbar));
+	s->set_whitebal(s, preferences.getInt("awb", s->status.awb));
+	s->set_gain_ctrl(s, preferences.getInt("agc", s->status.agc));
+	s->set_exposure_ctrl(s, preferences.getInt("aec", s->status.aec));
+	s->set_hmirror(s, preferences.getInt("hmirror", s->status.hmirror));
+	s->set_vflip(s, preferences.getInt("vflip", s->status.vflip));
+	s->set_awb_gain(s, preferences.getInt("awb_gain", s->status.awb_gain));
+	s->set_agc_gain(s, preferences.getInt("agc_gain", s->status.agc_gain));
+	s->set_aec_value(s, preferences.getInt("aec_value", s->status.aec_value));
+	s->set_aec2(s, preferences.getInt("aec2", s->status.aec2));
+	s->set_dcw(s, preferences.getInt("dcw", s->status.dcw));
+	s->set_bpc(s, preferences.getInt("bpc", s->status.bpc));
+	s->set_wpc(s, preferences.getInt("wpc", s->status.wpc));
+	s->set_raw_gma(s, preferences.getInt("raw_gma", s->status.raw_gma));
+	s->set_lenc(s, preferences.getInt("lenc", s->status.lenc));
+	s->set_special_effect(s, preferences.getInt("special_effect", s->status.special_effect));
+	s->set_wb_mode(s, preferences.getInt("wb_mode", s->status.wb_mode));
+	s->set_ae_level(s, preferences.getInt("ae_level", s->status.ae_level));
+
+	preferences.end();
+
 	ledcSetup(7, 5000, 8);
 	ledcAttachPin(4, 7);	//pin4 is LED
 	
@@ -769,6 +807,7 @@ void setup(){
 
 	WiFi.mode(WIFI_STA);
 	WiFi.begin(SSID1, PWD1);
+	WiFi.setSleep(false);
 	Serial.print("Connecting to WiFi");
 	while (WiFi.status() != WL_CONNECTED){
 		delay(500);
@@ -811,80 +850,160 @@ void loop() {
 		vTaskDelay(1000);
 }
 
-void set_handler(){
+void get_handler(){
 	sensor_t * s = esp_camera_sensor_get();
-	StaticJsonDocument<1000> data;
-	data["framesize"] = s->status.framesize;
-	data["quality"] = 63 - s->status.quality;
-	data["contrast"] = s->status.contrast;
-	data["brightness"] = s->status.brightness;
-	data["saturation"] = s->status.saturation;
-	data["gainceiling"] = s->status.gainceiling;
-	data["colorbar"] = s->status.colorbar;
-	data["awb"] = s->status.awb;
-	data["agc"] = s->status.agc;
-	data["aec"] = s->status.aec;
-	data["hmirror"]	=	s->status.hmirror;
-	data["vflip"] = s->status.vflip;
-	data["awb_gain"] = s->status.awb_gain;
-	data["agc_gain"] = s->status.agc_gain;
-	data["aec_value"] = s->status.aec_value;
-	data["aec2"] = s->status.aec2;
-	data["dcw"] = s->status.dcw;
-	data["bpc"] = s->status.bpc;
-	data["wpc"] = s->status.wpc;
-	data["raw_gma"] = s->status.raw_gma;
-	data["lenc"] = s->status.lenc;
-	data["special_effect"] = s->status.special_effect;
-	data["wb_mode"] = s->status.wb_mode;
-	data["ae_level"] = s->status.ae_level;
-
+	StaticJsonDocument<400> data;
+	
+	preferences.begin("CameraSettings", false);
+	
+	int framesize = preferences.getInt("framesize", 999);
+	data["framesize"] = (framesize == 999) ? s->status.framesize : framesize;
+	int quality = preferences.getInt("quality", 999);
+	data["quality"] = (quality == 999) ? 63 - s->status.quality : quality;
+	int contrast = preferences.getInt("contrast", 999);
+	data["contrast"] = (contrast == 999) ? s->status.contrast : contrast;
+	int brightness = preferences.getInt("brightness", 999);
+	data["brightness"] = (brightness == 999) ? s->status.brightness : brightness;
+	int saturation = preferences.getInt("saturation", 999);
+	data["saturation"] = (saturation == 999) ? s->status.saturation : saturation;
+	int gainceiling = preferences.getInt("gainceiling", 999);
+	data["gainceiling"] = (gainceiling == 999) ? s->status.gainceiling : gainceiling;
+	int colorbar = preferences.getInt("colorbar", 999);
+	data["colorbar"] = (colorbar == 999 ) ? s->status.colorbar : colorbar;
+	int awb = preferences.getInt("awb", 999);
+	data["awb"] = (awb == 999) ? s->status.awb : awb;
+	int agc = preferences.getInt("agc", 999);
+	data["agc"] = (agc == 999) ? s->status.agc : agc;
+	int aec = preferences.getInt("aec", 999);
+	data["aec"] = (aec == 999) ? s->status.aec : aec;
+	int hmirror = preferences.getInt("hmirror", 999);
+	data["hmirror"] = (hmirror == 999) ? s->status.hmirror : hmirror;
+	int vflip = preferences.getInt("vflip", 999);
+	data["vflip"] = (vflip == 999) ? s->status.vflip : vflip;
+	int awb_gain = preferences.getInt("awb_gain", 999);
+	data["awb_gain"] = (awb_gain == 999) ? s->status.awb_gain : awb_gain;
+	int agc_gain = preferences.getInt("agc_gain", 999);
+	data["agc_gain"] = (agc_gain == 999) ? s->status.agc_gain : agc_gain;
+	int aec_value = preferences.getInt("aec_value", 999);
+	data["aec_value"] = (aec_value == 999) ? s->status.aec_value : aec_value;
+	int aec2 = preferences.getInt("aec2", 999);
+	data["aec2"] = (aec2 == 999) ? s->status.aec2 : aec2;
+	int dcw = preferences.getInt("dcw", 999);
+	data["dcw"] = (dcw == 999) ? s->status.dcw : dcw;
+	int bpc = preferences.getInt("bpc", 999);
+	data["bpc"] = (bpc == 999) ? s->status.bpc : bpc;
+	int wpc = preferences.getInt("wpc", 999);
+	data["wpc"] = (wpc == 999) ? s->status.wpc : wpc;
+	int raw_gma = preferences.getInt("raw_gma", 999);
+	data["raw_gma"] = (raw_gma == 999) ? s->status.raw_gma : raw_gma;
+	int lenc = preferences.getInt("lenc", 999);
+	data["lenc"] = (lenc == 999) ? s->status.lenc : lenc;
+	int special_effect = preferences.getInt("special_effect", 999);
+	data["special_effect"] = (special_effect == 999) ? s->status.special_effect : special_effect;
+	int wb_mode = preferences.getInt("wb_mode", 999);
+	data["wb_mode"] = (wb_mode == 999) ? s->status.wb_mode : wb_mode;
+	int ae_level = preferences.getInt("ae_level", 999);
+	data["ae_level"] = (ae_level == 999) ? s->status.ae_level : ae_level;
+	
+	preferences.end();
+	
 	String response;
 	serializeJson(data, response);
 	server.send(200, "application/json", response);
 }
 
-void get_handler(){
+void set_handler(){
 	String variable = server.arg("var");
 	String value = server.arg("val");
  	
 		int val = value.toInt();
 		
 		sensor_t * s = esp_camera_sensor_get();
-		int res = 0;
+		
+		preferences.begin("CameraSettings", false);
 		
 		if(variable == "framesize") {
 				if(s->pixformat == PIXFORMAT_JPEG) 
-					res = s->set_framesize(s, (framesize_t)val);
+					s->set_framesize(s, (framesize_t)val);
+					preferences.putInt("framesize", val);
 		}
-		else if(variable == "quality") res = s->set_quality(s, 63 - val);
-		else if(variable == "contrast") res = s->set_contrast(s, val);
-		else if(variable == "brightness") res = s->set_brightness(s, val);
-		else if(variable == "saturation") res = s->set_saturation(s, val);
-		else if(variable == "gainceiling") res = s->set_gainceiling(s, (gainceiling_t)val);
-		else if(variable == "colorbar") res = s->set_colorbar(s, val);
-		else if(variable == "awb") res = s->set_whitebal(s, val);
-		else if(variable == "agc") res = s->set_gain_ctrl(s, val);
-		else if(variable == "aec") res = s->set_exposure_ctrl(s, val);
-		else if(variable == "hmirror") res = s->set_hmirror(s, val);
-		else if(variable == "vflip") res = s->set_vflip(s, val);
-		else if(variable == "awb_gain") res = s->set_awb_gain(s, val);
-		else if(variable == "agc_gain") res = s->set_agc_gain(s, val);
-		else if(variable == "aec_value") res = s->set_aec_value(s, val);
-		else if(variable == "aec2") res = s->set_aec2(s, val);
-		else if(variable == "dcw") res = s->set_dcw(s, val);
-		else if(variable == "bpc") res = s->set_bpc(s, val);
-		else if(variable == "wpc") res = s->set_wpc(s, val);
-		else if(variable == "raw_gma") res = s->set_raw_gma(s, val);
-		else if(variable == "lenc") res = s->set_lenc(s, val);
-		else if(variable == "special_effect") res = s->set_special_effect(s, val);
-		else if(variable == "wb_mode") res = s->set_wb_mode(s, val);
-		else if(variable == "ae_level") res = s->set_ae_level(s, val);
-
+		else if(variable == "quality"){
+			s->set_quality(s, 63 - val);
+			preferences.putInt("quality", val);
+		} else if(variable == "contrast"){
+			s->set_contrast(s, val);
+			preferences.putInt("contrast", val);
+		} else if(variable == "brightness"){
+			s->set_brightness(s, val);
+			preferences.putInt("brightness", val);
+		} else if(variable == "saturation"){
+			s->set_saturation(s, val);
+			preferences.putInt("saturation", val);
+		} else if(variable == "gainceiling"){
+			s->set_gainceiling(s, (gainceiling_t)val);
+			preferences.putInt("gainceiling", val);
+		} else if(variable == "colorbar"){
+			s->set_colorbar(s, val);
+			preferences.putInt("colorbar", val);
+		} else if(variable == "awb"){
+			s->set_whitebal(s, val);
+			preferences.putInt("awb", val);
+		} else if(variable == "agc"){
+			s->set_gain_ctrl(s, val);
+			preferences.putInt("agc", val);
+		} else if(variable == "aec"){
+			s->set_exposure_ctrl(s, val);
+			preferences.putInt("aec", val);
+		} else if(variable == "hmirror"){
+			s->set_hmirror(s, val);
+			preferences.putInt("hmirror", val);
+		} else if(variable == "vflip"){
+			s->set_vflip(s, val);
+			preferences.putInt("vflip", val);
+		} else if(variable == "awb_gain"){
+			s->set_awb_gain(s, val);
+			preferences.putInt("awb_gain", val);
+		} else if(variable == "agc_gain"){
+			s->set_agc_gain(s, val);
+			preferences.putInt("agc_gain", val);
+		} else if(variable == "aec_value"){
+			s->set_aec_value(s, val);
+			preferences.putInt("aec_value", val);
+		} else if(variable == "aec2"){
+			s->set_aec2(s, val);
+			preferences.putInt("aec2", val);
+		} else if(variable == "dcw"){
+			s->set_dcw(s, val);
+			preferences.putInt("dcw", val);
+		} else if(variable == "bpc"){
+			s->set_bpc(s, val);
+			preferences.putInt("bpc", val);
+		} else if(variable == "wpc"){
+			s->set_wpc(s, val);
+			preferences.putInt("wpc", val);
+		} else if(variable == "raw_gma"){
+			s->set_raw_gma(s, val);
+			preferences.putInt("raw_gma", val);
+		} else if(variable == "lenc"){
+			s->set_lenc(s, val);
+			preferences.putInt("lenc", val);
+		} else if(variable == "special_effect"){
+			s->set_special_effect(s, val);
+			preferences.putInt("special_effect", val);
+		} else if(variable == "wb_mode"){
+			s->set_wb_mode(s, val);
+			preferences.putInt("wb_mode", val);
+		} else if(variable == "ae_level"){
+			s->set_ae_level(s, val);
+			preferences.putInt("ae_level", val);
+		}
+		
+		preferences.end();
+		
 		server.send(200, "text/plain", ("OK"));
 }
 
-void (){
+void control3_handler(){
 	server.send(200, "text/html", (const char *)INDEX_HTML);
 }
 
@@ -897,4 +1016,14 @@ void restart_handler(){
 void activatewebota_handler(){
 	flag = true;
 	server.send(200, "text/plain", ("OK"));
+}
+
+void reset_handler(){
+	preferences.begin("CameraSettings", false);
+	preferences.clear();
+	preferences.end();
+	server.send(200, "text/plain", ("OK"));
+	server.send(200, "text/plain", ("OK"));
+	delay(500);
+	ESP.restart();
 }
